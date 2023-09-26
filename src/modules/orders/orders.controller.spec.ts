cfa74b6767baaedeb88e868b3aaa4b5ae9dd59cf
Oluwadara Oloye <daraoloye99@gmail.com>
2023-09-27 00:15:38 +0100

    chore: add test dependencies

--- src/modules/orders/orders.controller.spec.ts
+++ src/modules/orders/orders.controller.spec.ts
@@ -4,14 +4,29 @@ import { OrdersService } from './orders.service';
 
 describe('OrdersController', () => {
   let controller: OrdersController;
+  let ordersService: OrdersService;
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrdersController],
-      providers: [OrdersService],
+      providers: [
+        {
+          provide: OrdersService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findAllOrdersforUser: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          }
+        }
+      ],
     }).compile();
 
     controller = module.get<OrdersController>(OrdersController);
+    ordersService = module.get<OrdersService>(OrdersService);
   });
 
   it('should be defined', () => {
--- src/modules/orders/orders.service.spec.ts
+++ src/modules/orders/orders.service.spec.ts
@@ -1,15 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersService } from './orders.service';
+import { Repository } from 'typeorm';
+import { Order } from 'src/entities/order.entity';
+import { UsersService } from '../users/users.service';
+import { CartsService } from '../carts/carts.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('OrdersService', () => {
   let service: OrdersService;
+  let orderRepository: Repository<Order>;
+  let usersService: UsersService;
+  let cartService: CartsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrdersService],
+      providers: [
+        OrdersService,
+        {
+          provide: getRepositoryToken(Order),
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          }
+        },
+        {
+          provide: CartsService,
+          useValue: {
+            findOne: jest.fn(),
+          }
+        },
+        {
+          provide: UsersService,
+          useValue: {
+            findOne: jest.fn(),
+          }
+        },
+      ],
     }).compile();
 
     service = module.get<OrdersService>(OrdersService);
+    orderRepository = module.get<Repository<Order>>(getRepositoryToken(Order));
+    usersService = module.get<UsersService>(UsersService);
+    cartService = module.get<CartsService>(CartsService);
   });
 
   it('should be defined', () => {
